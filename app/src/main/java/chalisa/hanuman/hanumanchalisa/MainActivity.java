a7e3ace1f82b606259b5f91ce97c173f68826ed7
arpit0303 <arpitagrawal910@gmail.com>
2016-09-20 20:31:26 +0530

    Change behaviour of MediaPlayer

--- app/src/main/java/chalisa/hanuman/hanumanchalisa/MainActivity.java
+++ app/src/main/java/chalisa/hanuman/hanumanchalisa/MainActivity.java
@@ -9,23 +9,22 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.startapp.android.publish.StartAppAd;
 import com.startapp.android.publish.StartAppSDK;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     ImageButton play;
     ImageButton reload;
     Button chalisa;
     ImageView hanumanImage;
-    TextView  chalisaText;
-    static int click = 0;
+    TextView chalisaText;
+    static int click = -1;
     static int i = 0;
     MediaPlayer mediaPlayer;
     private StartAppAd startAppAd = new StartAppAd(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,71 +33,81 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         startAppAd.showAd(); // show the ad
-        startAppAd.loadAd();
-
-        StartAppAd.showSlider(this);
-
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
 
         play = (ImageButton) findViewById(R.id.start);
-        reload = (ImageButton) findViewById(R.id.stop);
+        reload = (ImageButton) findViewById(R.id.reload);
         chalisa = (Button) findViewById(R.id.chalisa);
         hanumanImage = (ImageView) findViewById(R.id.hanuman_bgImage);
         chalisaText = (TextView) findViewById(R.id.chalisaText);
 
-        chalisa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(i%2 == 0) {
-                    hanumanImage.setAlpha((float) 0.3);
-                    chalisaText.setText(R.string.chalisa);
-                    i++;
-                }else{
-                    hanumanImage.setAlpha((float) 1);
-                    chalisaText.setText("");
-                    i++;
-                }
-            }
-        });
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.hanuman_chalisa);
 
-         mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.hanuman_chalisa);
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(click%2 == 0) {
-                    mediaPlayer.start();
-                    play.setImageResource(R.drawable.ic_action_pause);
-                    click++;
-
-                }else{
-                    mediaPlayer.pause();
-                    play.setImageResource(R.drawable.ic_action_play);
-                    click++;
-                }
-            }
-        });
-        reload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(mediaPlayer.isPlaying()) {
-                    mediaPlayer.stop();
-                    mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.hanuman_chalisa);
-                    play.setImageResource(R.drawable.ic_action_play);
-                    click = 0;
-                }
-            }
-        });
+        chalisa.setOnClickListener(this);
+        play.setOnClickListener(this);
+        reload.setOnClickListener(this);
     }
+
     @Override
-    public void onResume() {
-        super.onResume();
-        startAppAd.onResume();
+    protected void onPause() {
+        super.onPause();
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (click % 2 == 0) {
+            mediaPlayer.start();
+        }
+    }
+
     @Override
     public void onBackPressed() {
         startAppAd.onBackPressed();
         super.onBackPressed();
+
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            click = -1;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.chalisa:
+                if (i % 2 == 0) {
+                    hanumanImage.setAlpha((float) 0.3);
+                    chalisaText.setText(R.string.chalisa);
+                    i++;
+                } else {
+                    hanumanImage.setAlpha((float) 1);
+                    chalisaText.setText("");
+                    i++;
+                }
+                break;
+            case R.id.start:
+                click++;
+                if (click % 2 == 0) {
+                    mediaPlayer.start();
+                    play.setImageResource(R.drawable.ic_action_pause);
+                } else {
+                    mediaPlayer.pause();
+                    play.setImageResource(R.drawable.ic_action_play);
+                }
+                break;
+            case R.id.reload:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.hanuman_chalisa);
+                    play.setImageResource(R.drawable.ic_action_play);
+                    click = 0;
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
